fe58e65c9393fff9985b49fc3e9cb93d9937526e
Mariusz Bernacki <github@didalgo.com>
2023-06-13 22:54:17 +0200

    Add support for `0613 models

--- src/main/java/com/didalgo/gpt3/ChatFormatDescriptor.java
+++ src/main/java/com/didalgo/gpt3/ChatFormatDescriptor.java
@@ -21,10 +21,12 @@ public interface ChatFormatDescriptor {
 
     static ChatFormatDescriptor forModel(String modelName) {
         return switch (modelName) {
-            case "gpt-3.5-turbo" -> forModel("gpt-3.5-turbo-0301");
-            case "gpt-4" -> forModel("gpt-4-0314");
-            case "gpt-3.5-turbo-0301" -> new Of(Encoding.forModel(modelName), 4, 3);
-            case "gpt-4-0314" -> new Of(Encoding.forModel(modelName), 3, 3);
+            case "gpt-3.5-turbo", "gpt-3.5-turbo-16k" -> forModel("gpt-3.5-turbo-0301");
+            case "gpt-4", "gpt-4-32k" -> forModel("gpt-4-0314");
+            case "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613",
+                    "gpt-3.5-turbo-16k-0613" -> new Of(Encoding.forModel(modelName), 4, 3);
+            case "gpt-4-0314", "gpt-4-0613",
+                    "gpt-4-32k-0314", "gpt-4-32k-0613" -> new Of(Encoding.forModel(modelName), 3, 3);
             default -> throw new IllegalArgumentException(String.format("Model `%s` not found", modelName));
         };
     }
