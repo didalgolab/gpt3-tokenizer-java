7a3d8b8e3e5ff13291c22719a1d281b8375dfcef
Mariusz Bernacki <github@didalgo.com>
2023-06-15 00:38:42 +0200

    Fix support for `0613 models

--- src/main/java/com/didalgo/gpt3/ChatFormatDescriptor.java
+++ src/main/java/com/didalgo/gpt3/ChatFormatDescriptor.java
@@ -4,6 +4,7 @@
  */
 package com.didalgo.gpt3;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 /**
@@ -21,11 +22,11 @@ public interface ChatFormatDescriptor {
 
     static ChatFormatDescriptor forModel(String modelName) {
         return switch (modelName) {
-            case "gpt-3.5-turbo", "gpt-3.5-turbo-16k" -> forModel("gpt-3.5-turbo-0301");
-            case "gpt-4", "gpt-4-32k" -> forModel("gpt-4-0314");
-            case "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613",
-                    "gpt-3.5-turbo-16k-0613" -> new Of(Encoding.forModel(modelName), 4, 3);
-            case "gpt-4-0314", "gpt-4-0613",
+            case "gpt-3.5-turbo" -> LocalDate.now().isBefore(LocalDate.of(2023, 6, 27))? forModel("gpt-3.5-turbo-0301") : forModel("gpt-3.5-turbo-0613");
+            case "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k" -> forModel("gpt-4-0314");
+            case "gpt-3.5-turbo-0301" -> new Of(Encoding.forModel(modelName), 4, 3);
+            case "gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k-0613",
+                    "gpt-4-0314", "gpt-4-0613",
                     "gpt-4-32k-0314", "gpt-4-32k-0613" -> new Of(Encoding.forModel(modelName), 3, 3);
             default -> throw new IllegalArgumentException(String.format("Model `%s` not found", modelName));
         };
--- src/test/java/com/didalgo/gpt3/TokenCountTest.java
+++ src/test/java/com/didalgo/gpt3/TokenCountTest.java
@@ -1,6 +1,9 @@
 package com.didalgo.gpt3;
 
+import com.theokanning.openai.completion.chat.ChatMessage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -16,5 +19,25 @@ public class TokenCountTest {
         assertEquals(1, TokenCount.fromLinesJoined(List.of("1"), tokenizer));
         assertEquals(3, TokenCount.fromLinesJoined(List.of("1", "2"), tokenizer));
         assertEquals(5, TokenCount.fromLinesJoined(List.of("1", "2", "3"), tokenizer));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "121, gpt-3.5-turbo-0301",
+            "115, gpt-3.5-turbo-0613",
+            "115, gpt-3.5-turbo-16k-0613",
+            "115, gpt-4-0314",
+            "115, gpt-4-0613"
+    })
+    void fromMessages_gives_correct_token_count(int expectedTokenCount, String modelName) {
+        List<ChatMessage> messages = List.of(
+                new ChatMessage("system", "You are a helpful, pattern-following assistant that translates corporate jargon into plain English."),
+                new ChatMessage("user", "New synergies will help drive top-line growth."),
+                new ChatMessage("assistant", "Things working well together will increase revenue."),
+                new ChatMessage("user", "Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage."),
+                new ChatMessage("assistant", "Let's talk later when we're less busy about how to do better."),
+                new ChatMessage("user", "This late pivot means we don't have time to boil the ocean for the client deliverable.")
+        );
+        assertEquals(expectedTokenCount, TokenCount.fromMessages(messages, tokenizer, ChatFormatDescriptor.forModel(modelName)));
     }
 }
